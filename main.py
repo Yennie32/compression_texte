c37615f140229203739d9898661db305940b5b61
Your Name <you@example.com>
2024-10-24 20:25:02 +0200

    text joined - Step 1 done

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+text = "qu'elle est... » ). expert en utilisabilite des"
+
 def splitting_text (text) :
     split_text = text.split()
     return split_text
 
-print(splitting_text(text = "qu'elle est... » ). expert en utilisabilite des"))
+splitted = splitting_text(text)
+
+def joining_text(text):
+    join_text = " ".join(text)
+    return join_text
+
+
+print(splitting_text(text))
+print(joining_text(splitted))
